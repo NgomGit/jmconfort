1e790468797db226e0d3301b184656ac351f2aac
Mouhamed Lamine Ngom <ngommouhamedlamine95@gmail.com>
2025-03-01 20:25:03 +0000

    Saving changes regarding page and styles

--- pages/_document.js
+++ pages/_document.js
@@ -15,9 +15,9 @@ export default class MyDocument extends Document {
                     <link rel="icon" href="favicon.png" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,500,600,700,800%7CPoppins:200,300,400,500,600,700,800%7COswald:300,400,600,700" />
                     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-                    <link rel="stylesheet" type="text/css" href="vendor/bootstrap.min.css" />
-                    <link rel="stylesheet" type="text/css" href="vendor/fontawesome-free/css/all.min.css" />
-                    <link rel="stylesheet" type="text/css" href="vendor/simple-line-icons/css/simple-line-icons.min.css" />
+                    <link rel="stylesheet" type="text/css" href="/vendor/bootstrap.min.css" />
+                    <link rel="stylesheet" type="text/css" href="/vendor/fontawesome-free/css/all.min.css" />
+                    <link rel="stylesheet" type="text/css" href="/vendor/simple-line-icons/css/simple-line-icons.min.css" />
                 </Head>
                 <body>
                     <Main />
